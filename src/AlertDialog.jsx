6c98b1dfb620eada5c303c83e511836c0a04e6fb
Vivekanand P V <vivekanandpv@outlook.com>
2023-08-11 12:38:25 +0530

    dialog implemented

---
+++ src/AlertDialog.jsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+const AlertDialog = (props) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    props.onChoose('closed without a response');
+  };
+
+  const handleAgree = () => {
+    setOpen(false);
+    props.onChoose('agree');
+  };
+
+  const handleDisagree = () => {
+    setOpen(false);
+    props.onChoose('disagree');
+  };
+
+  return (
+    <div>
+      <Button variant='outlined' onClick={handleClickOpen}>
+        Open alert dialog
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='alert-dialog-title'
+        aria-describedby='alert-dialog-description'
+      >
+        <DialogTitle id='alert-dialog-title'>
+          {"Use Google's location service?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id='alert-dialog-description'>
+            Let Google help apps determine location. This means sending
+            anonymous location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDisagree}>Disagree</Button>
+          <Button onClick={handleAgree} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default AlertDialog;
--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import SampleForm from './SampleForm';
 import MuiDataGrid from './MuiDataGrid';
 import MuiCharts from './MuiCharts';
 import AgGrid from './AgGrid';
+import AlertDialog from './AlertDialog';
 
 const App = (props) => {
+  const [dialogResponse, setDialogResponse] = React.useState('No Response');
   return (
     <>
       <div className='p-5'>
@@ -13,6 +15,8 @@ const App = (props) => {
         {/* <MuiDataGrid /> */}
 
         {/* <MuiCharts /> */}
+        <AlertDialog onChoose={setDialogResponse} />
+        <p>Alert dialog response: {dialogResponse}</p>
         <AgGrid />
       </div>
     </>
